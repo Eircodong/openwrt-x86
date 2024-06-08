fb4c3b1865fa7c42a2fee4701d71a9263fb853bb
EircoD <59461660+Eircodong@users.noreply.github.com>
2024-06-08 18:59:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 #!/bin/bash
 
 # 添加软件源
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
