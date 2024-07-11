c47cf17aa5686c7ad8e5ec800840598320dcef08
EircoD <59461660+Eircodong@users.noreply.github.com>
2024-07-11 17:42:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -40,3 +40,4 @@ sed -i "s/${orig_version}/R${date_version} by EircoD/g" package/lean/default-set
 
 # 删除重复插件
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/packages/net/adguardhome
