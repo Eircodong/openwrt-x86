1e37bb7cbfec53bb5367a5b66c2d3fac741cdfff
EircoD <59461660+Eircodong@users.noreply.github.com>
2024-08-29 11:10:16 +0800

    Update x86immdiy-part2.sh

--- x86immdiy-part2.sh
+++ x86immdiy-part2.sh
@@ -33,6 +33,11 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-l
 
 # 添加插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # 删除重复插件
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 # rm -rf feeds/smpackage/adguardhome
