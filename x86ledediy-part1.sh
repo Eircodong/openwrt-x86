393e5eb1291cfc9581e58cbdf0971cbfcd05d793
EircoD <59461660+Eircodong@users.noreply.github.com>
2024-09-19 16:27:40 +0800

    Update x86ledediy-part1.sh

--- x86ledediy-part1.sh
+++ x86ledediy-part1.sh
@@ -17,3 +17,6 @@
 
 # 添加软件源
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# 切换luci版本
+sed -i 's/src-git luci https://github.com/coolsnowwolf/luci/#src-git luci https://github.com/coolsnowwolf/luci/g'feeds.conf.default
+sed -i '#src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05/src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05/g'feeds.conf.default
