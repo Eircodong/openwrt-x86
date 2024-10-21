5f07c07fbf6e243cfc69b4fce611a0aa51e7905c
EircoD <59461660+Eircodong@users.noreply.github.com>
2024-10-21 14:44:12 +0800

    Update x86ledediy-part2.sh

--- x86ledediy-part2.sh
+++ x86ledediy-part2.sh
@@ -37,6 +37,7 @@ git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app
 git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+git clone --depth=1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git_sparse_clone master https://github.com/coolsnowwolf/luci applications/luci-app-uugamebooster
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 git_sparse_clone openwrt-23.05 https://github.com/immortalwrt/luci applications/luci-app-eqos
