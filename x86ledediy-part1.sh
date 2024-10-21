d2e1f993ee96a41658774e400e965d7362043aac
EircoD <59461660+Eircodong@users.noreply.github.com>
2024-10-21 14:41:28 +0800

    Update x86ledediy-part1.sh

--- x86ledediy-part1.sh
+++ x86ledediy-part1.sh
@@ -16,4 +16,8 @@
 #!/bin/bash
 
 # 添加软件源
-
+echo >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
