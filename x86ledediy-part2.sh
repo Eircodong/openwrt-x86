e921bcbfda65d8a253ef93eb1a3da6360752845e
EircoD <59461660+Eircodong@users.noreply.github.com>
2024-10-21 14:51:15 +0800

    Update x86ledediy-part2.sh

--- x86ledediy-part2.sh
+++ x86ledediy-part2.sh
@@ -44,9 +44,6 @@ git_sparse_clone openwrt-23.05 https://github.com/immortalwrt/luci applications/
 
 # 科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 
 # SmartDNS
 git clone --depth=1 -b master https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
