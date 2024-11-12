ae946aa45cc856690e6df57761fda40c6419d705
EircoD <59461660+Eircodong@users.noreply.github.com>
2024-11-12 10:55:23 +0800

    Update x86ledediy-part2.sh

--- x86ledediy-part2.sh
+++ x86ledediy-part2.sh
@@ -11,8 +11,7 @@
 #
 
 # 设置默认ip
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/bin/config_generate
 
 # 设置默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
